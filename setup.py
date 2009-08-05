571df71a96fe3817a2f47449a4d0d50a60f6063f
Julian Infanger <j.infanger@4teamwork.ch>
2009-08-05 14:57:31 +0000

    moved version from setup.py to version.txt

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 import os
 
-version = '0.2-dev'
+version = open('izug/tagging/version.txt').read().strip()
 maintainer = 'Mathias Leimgruber'
 
 setup(name='izug.tagging',
