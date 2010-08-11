b3d5294ffe9ffe422f2fa8763c253c8f9783e343
Philippe Gross <p.gross@4teamwork.ch>
2010-08-11 17:51:40 +0200

    fixed KeywordWidget Handling for the tags field

---
+++ ftw/tagging/skins/ftw_tagging/collectKeywords.py
@@ -0,0 +1,34 @@
+## Script (Python) "collectKeywords"
+##title=Edit content
+##bind container=container
+##bind context=context
+##bind namespace=
+##bind script=script
+##bind subpath=traverse_subpath
+##parameters=name, index, vocab_source='portal_catalog'
+REQUEST=context.REQUEST
+
+
+## With the advent of multi-cataloging we need to pass an optional
+## catalog id to use for the widget.
+
+allowed, enforce = context.Vocabulary(name)
+
+
+# special handling for ftw.tagging
+# only return tags from the vocabulary, but we can't set enforceVocabulary
+# because it must be possible to add new ones
+
+if vocab_source == 'special_tag_source':
+    result = allowed
+else:
+    catalog = getattr(container, vocab_source)
+    previous = catalog.uniqueValuesFor(index)
+    if enforce:
+        result = allowed
+    else:
+        result = allowed + previous
+
+result = result.sortedByValue()
+
+return result
