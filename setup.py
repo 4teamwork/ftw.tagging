0c1170402a82b61bef0d26f30f0cfda2eaa55991
Jonas Baumann <jone@jone.ch>
2013-01-28 11:22:30 +0100

    Update classifiers.

--- setup.py
+++ setup.py
@@ -19,11 +19,14 @@ setup(name='ftw.tagging',
 
       # Get more strings from
       # http://www.python.org/pypi?%3Aaction=list_classifiers
+
       classifiers=[
-        "Framework :: Plone",
-        "Framework :: Plone :: 4.0",
-        "Programming Language :: Python",
-        "Topic :: Software Development :: Libraries :: Python Modules",
+        'Framework :: Plone',
+        'Framework :: Plone :: 4.0',
+        'Framework :: Plone :: 4.1',
+        'Framework :: Plone :: 4.2',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules',
         ],
 
       keywords='ftw tagging plone',
