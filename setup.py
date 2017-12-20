621c0e23b53f9e6d217ccfe9e8141c266a0dbb68
Martin Bächtold <martin@baechtold.me>
2017-12-20 15:47:23 +0100

    Remove Trove classifier (Plone 4.1).
    Support for Plone 4.1 has been dropped a while ago.

--- setup.py
+++ setup.py
@@ -25,7 +25,6 @@ setup(name='ftw.tagging',
 
       classifiers=[
         'Framework :: Plone',
-        'Framework :: Plone :: 4.1',
         'Framework :: Plone :: 4.2',
         'Framework :: Plone :: 4.3',
         'Programming Language :: Python',
