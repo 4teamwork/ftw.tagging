a5e2329556eb3aa5eec7f06345e50a7e880b66a0
Jonas Baumann <jone@jone.ch>
2019-11-12 16:18:27 +0100

    Use old version of ftw.testing.
    Downgrad ftw.testing so that mock tests still work.

--- ftw/tagging/tests/test_uninstall.py
+++ ftw/tagging/tests/test_uninstall.py
@@ -1,8 +1,8 @@
 from ftw.testing import IS_PLONE_5
 from ftw.testing.genericsetup import GenericSetupUninstallMixin
 from ftw.testing.genericsetup import apply_generic_setup_layer
-from unittest2 import TestCase
-from unittest2 import skipUnless
+from unittest import TestCase
+from unittest import skipUnless
 
 
 @apply_generic_setup_layer
